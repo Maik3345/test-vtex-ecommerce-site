72f9e34e894a772e814f9b5c11100675fb84825d
Michael Stiven Restrepo Alvarez <michael.restrepo@puntoscolombia.com>
2024-11-08 10:49:41 -0500

    update .gitignore to exclude dist directory; modify Vite config for GitHub Pages deployment; enhance package.json scripts for build and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://maik3345.github.io/test-vtex-ecommerce-site",
+});
